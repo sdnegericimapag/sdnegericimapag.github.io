3081f10d82aec151fff011215bcad61b8c225116
SD NEGERI CIMAPAG <sdnegericimapag37@gmail.com>
2026-02-15 22:42:36 +0700

    Add files via upload

---
+++ js/saran.js
@@ -0,0 +1,4 @@
+document.getElementById("popup-saran").addEventListener("click", function() {
+  // arahkan ke index.html dan langsung ke section #saran
+  window.location.href = "index.html#saran";
+});
